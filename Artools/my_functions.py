4d216903edea6d32130da925cf2cddc44f569be9
Arthur Prigent <45233171+aprig@users.noreply.github.com>
2023-04-22 17:38:43 +0200

    Update my_functions.py

--- Artools/my_functions.py
+++ Artools/my_functions.py
@@ -86,40 +86,7 @@ def ano_norm_t(ds):
     clim     = ds.groupby('time.month').mean('time')
     clim_std = ds.groupby('time.month').std('time')
     ano      = ds.groupby('time.month') - clim
-    ano_norm = xr.apply_ufunc(lambda x, m, s: (x - m) / s,
-                                    ds.groupby('time.month'),
-                                    clim, clim_std)
-    
-    return ano, ano_norm
 
-def ano_norm_tc(ds):
     
-    '''Compute the anomalies by removing the monthly means. 
-    The anomalies are normalized by their corresponding month.
-    
-    Parameters
-    ----------
-    
-    ds : xarray_like
-    Timeserie or 3d field.
-    
-    Returns
-    -----------
-    
-    ano : xarray_like
-    Returns the anomalies of var relative the climatology.
-    
-    ano_norm : xarray_like
-    Returns the anomalies of var relative the climatology normalized by the standard deviation.
-    
-    '''
-    
-    
-    clim     = ds.groupby('time_counter.month').mean('time_counter')
-    clim_std = ds.groupby('time_counter.month').std('time_counter')
-    ano      = ds.groupby('time_counter.month') - clim
-    ano_norm = xr.apply_ufunc(lambda x, m, s: (x - m) / s,
-                                    ds.groupby('time_counter.month'),
-                                    clim, clim_std)
-    
-    return ano, ano_norm
+    return ano
+
